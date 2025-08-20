ab77e6cd916f26aeefe043792ad3015cc438f872
Ashley K Motsie <motsieashley31@gmail.com>
2025-08-20 12:05:38 +0200

    Home page updates commit

--- script.js
+++ script.js
@@ -187,11 +187,11 @@ function initParticles() {
   particlesJS("particles-js", {
     particles: {
       number: { value: 60, density: { enable: true, value_area: 800 } },
-      color: { value: "#3b82f6" },
+      color: { value: "#00ff11" },
       shape: { type: "circle" },
       opacity: { value: 0.5, random: true, anim: { enable: true, speed: 1, opacity_min: 0.1 } },
       size: { value: 3, random: true, anim: { enable: true, speed: 5, size_min: 0.1 } },
-      line_linked: { enable: true, distance: 150, color: "#2563eb", opacity: 0.4, width: 1 },
+      line_linked: { enable: true, distance: 150, color: "#00ff11", opacity: 0.4, width: 1 },
       move: { enable: true, speed: 4, direction: "none", random: true, straight: false, out_mode: "out" }
     },
     interactivity: {
